dac5d0067e603ac89e31460b052960113f556180
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-04-25 21:50:22 +0000

    some tests

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,24 @@
 <?php
+$db = new SQLite3('test.sqlite', SQLITE3_OPEN_READWRITE);
+
+// Create a table.
+$db->query(
+'CREATE TABLE IF NOT EXISTS "users" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "name" VARCHAR
+  )'
+);
+
+// Insert some sample data.
+$db->query('INSERT INTO "users" ("name") VALUES ("Karl")');
+$db->query('INSERT INTO "users" ("name") VALUES ("Linda")');
+$db->query('INSERT INTO "users" ("name") VALUES ("John")');
+
+// Get a count of the number of users
+$userCount = $db->querySingle('SELECT COUNT(DISTINCT "id") FROM "users"');
+echo("User count: $userCount\n");
+
+// Close the connection
+$db->close();
 echo "Si tu me vois ça marche mon reuf";
 ?>
