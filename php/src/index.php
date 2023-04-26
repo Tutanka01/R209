8ed6b34286ae540a382a2a8062af3c5392163305
poultier <Julien.poultier40000@gmail.com>
2023-04-26 13:31:05 +0200

    php modif

--- php/src/index.php
+++ php/src/index.php
@@ -3,7 +3,7 @@
 $db = new SQLite3('/var/www/html/test.sqlite', SQLITE3_OPEN_READWRITE); // Il faut imperativement que le fichier soit en .sqlite
 
 // Get a count of the number of users
-$compo = $db->querySingle('SELECT * FROM machine');
+$compo = $db->querySingle('SELECT * FROM composant');
 echo($compo);
 
 // Close the connection
