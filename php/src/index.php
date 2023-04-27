0eb62088eef289a5359c3ba67184909b83083616
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-04-27 08:45:50 +0000

    ajout des plats

--- php/src/index.php
+++ php/src/index.php
@@ -1,12 +1,12 @@
 <?php
 // Exemple de comment utiliser une database en php, ça ma pris 2h pour trouver comment faire :)
-$db = new SQLite3('/var/www/html/test.sqlite', SQLITE3_OPEN_READWRITE); // Il faut imperativement que le fichier soit en .sqlite
+//$db = new SQLite3('/var/www/html/test.sqlite', SQLITE3_OPEN_READWRITE); // Il faut imperativement que le fichier soit en .sqlite
 
 // Get a count of the number of users
-$compo = $db->querySingle('SELECT * FROM composant');
-echo($compo);
+//$compo = $db->querySingle('SELECT * FROM composant');
+//echo($compo);
 
 // Close the connection
-$db->close();
-include ("/var/www/html/static/mainapage.html");
+//$db->close();
+include ("/var/www/html/static/categorie.html");
 ?>
