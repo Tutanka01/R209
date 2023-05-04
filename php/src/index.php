e174d8b049144dd85d39de978553eab93c028add
Tutanka01 <40025011+Tutanka01@users.noreply.github.com>
2023-05-04 15:12:43 +0000

    je detest php

--- php/src/index.php
+++ php/src/index.php
@@ -1,20 +1,24 @@
 <?php
 // Exemple de comment utiliser une database en php, ça ma pris 2h pour trouver comment faire :)
 $db = new SQLite3('/var/www/html/sqlite.sqlite', SQLITE3_OPEN_READWRITE); // Il faut imperativement que le fichier soit en .sqlite
-
 // Get a count of the number of users
 $compo = $db->querySingle('SELECT * FROM composant');
 echo($compo);
 // Close the connection
 $db->close();
 
-
-include ("/var/www/html/static/mainapage.html");
-
+$categories = ['Caud', 'Flat', 'Entree', 'Plat', 'Dessert','Accompagnement'];
 if (isset($_POST['plat'])) {
     $plat = $_POST['plat'];
-    !unlink();
     include ("/var/www/html/static/plat.html");
+} elseif (isset($_POST['attribut'])) {  // 
+    $categorie = $_POST['value'];
+    echo($categorie);
+    include ("/var/www/html/static/categorie.html");
+} 
+else {
+    include ("/var/www/html/static/mainapage.html");
 }
 
+exit(); // arrête l'exécution du script après l'inclusion de la page "plat.html"
 ?>
